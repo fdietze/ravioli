33ca4073c06e1b89039e94be0e29e57186dc5c9b
Felix Dietze <felix.dietze@rwth-aachen.de>
2020-12-28 17:21:24 +0100

    Tatoeba: disable unnecessary imports

--- create_tatoeba_translation_db.sh
+++ create_tatoeba_translation_db.sh
@@ -19,7 +19,7 @@ SQLITEDB="$TRANS_OUT/translations.sqlite"
 # (cd "$TRANS_OUT"; wget --no-verbose --show-progress --timestamping "https://downloads.tatoeba.org/exports/users_sentences.csv")
 
 
-#echo "extracting and normalizing unicode..."
+echo "extracting and normalizing unicode..."
 (tar xOjf "$TRANS_OUT/sentences_detailed.tar.bz2" sentences_detailed.csv | ./normalize_unicode.sh > "$TRANS_OUT/sentences.tsv")
 (tar xOjf "$TRANS_OUT/links.tar.bz2" links.csv > "$TRANS_OUT/links.tsv")
 #(tar xOjf "$TRANS_OUT/tags.tar.bz2" tags.csv > "$TRANS_OUT/tags.tsv")
@@ -96,77 +96,77 @@ EOF
 
 
 # separate session to ignore foreign key errors
-cat << EOF | sqlite3 -init "" "$SQLITEDB" 2> /dev/null || true
-$SQLITE_INIT
+# cat << EOF | sqlite3 -init "" "$SQLITEDB" 2> /dev/null || true
+# $SQLITE_INIT
 
-SELECT "importing tags...";
-CREATE TABLE tags(
-  sentenceid INTEGER NOT NULL,
-  tag TEXT NOT NULL,
+# SELECT "importing tags...";
+# CREATE TABLE tags(
+#   sentenceid INTEGER NOT NULL,
+#   tag TEXT NOT NULL,
 
-  PRIMARY KEY (sentenceid, tag)
-  FOREIGN KEY (sentenceid)
-      REFERENCES sentences (sentenceid)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE
-);
+#   PRIMARY KEY (sentenceid, tag)
+#   FOREIGN KEY (sentenceid)
+#       REFERENCES sentences (sentenceid)
+#       ON UPDATE CASCADE
+#       ON DELETE CASCADE
+# );
 
-.mode ascii
-.separator "\t" "\n"
-.import '$TRANS_OUT/tags.tsv' tags
+# .mode ascii
+# .separator "\t" "\n"
+# .import '$TRANS_OUT/tags.tsv' tags
 
--- CREATE INDEX tags_sentenceid_idx ON tags (sentenceid);
--- CREATE INDEX tags_tag_idx ON tags (tag);
-EOF
-
-
-# separate session to ignore foreign key errors
-cat << EOF | sqlite3 -init "" "$SQLITEDB" 2> /dev/null || true
-$SQLITE_INIT
-
-SELECT "importing sentences_with_audio...";
-CREATE TABLE sentences_with_audio(
-  sentenceid INTEGER NOT NULL,
-  username TEXT NOT NULL,
-  license TEXT NOT NULL,
-  attribution_url TEXT NOT NULL,
-
-  FOREIGN KEY (sentenceid)
-      REFERENCES sentences (sentenceid)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE
-);
-
-.mode ascii
-.separator "\t" "\n"
-.import '$TRANS_OUT/sentences_with_audio.tsv' sentences_with_audio
-
--- CREATE INDEX sentences_with_audio_sentenceid_idx ON sentences_with_audio (sentenceid);
--- CREATE INDEX sentences_with_audio_username_idx ON sentences_with_audio (username);
--- CREATE INDEX sentences_with_audio_license_idx ON sentences_with_audio (license);
-EOF
+# -- CREATE INDEX tags_sentenceid_idx ON tags (sentenceid);
+# -- CREATE INDEX tags_tag_idx ON tags (tag);
+# EOF
 
 
 # separate session to ignore foreign key errors
-cat << EOF | sqlite3 -init "" "$SQLITEDB" 2> /dev/null || true
-$SQLITE_INIT
+# cat << EOF | sqlite3 -init "" "$SQLITEDB" 2> /dev/null || true
+# $SQLITE_INIT
 
-SELECT "importing user_languages...";
-CREATE TABLE user_languages(
-  lang TEXT NOT NULL,
-  skill_level INTEGER NOT NULL,
-  username TEXT NOT NULL,
-  details TEXT
-);
+# SELECT "importing sentences_with_audio...";
+# CREATE TABLE sentences_with_audio(
+#   sentenceid INTEGER NOT NULL,
+#   username TEXT NOT NULL,
+#   license TEXT NOT NULL,
+#   attribution_url TEXT NOT NULL,
 
-.mode ascii
-.separator "\t" "\n"
-.import '$TRANS_OUT/user_languages.tsv' user_languages
+#   FOREIGN KEY (sentenceid)
+#       REFERENCES sentences (sentenceid)
+#       ON UPDATE CASCADE
+#       ON DELETE CASCADE
+# );
 
--- CREATE INDEX user_languages_lang_idx ON user_languages (lang);
--- CREATE INDEX user_languages_skill_level_idx ON user_languages (skill_level);
--- CREATE INDEX user_languages_username_idx ON user_languages (username);
-EOF
+# .mode ascii
+# .separator "\t" "\n"
+# .import '$TRANS_OUT/sentences_with_audio.tsv' sentences_with_audio
+
+# -- CREATE INDEX sentences_with_audio_sentenceid_idx ON sentences_with_audio (sentenceid);
+# -- CREATE INDEX sentences_with_audio_username_idx ON sentences_with_audio (username);
+# -- CREATE INDEX sentences_with_audio_license_idx ON sentences_with_audio (license);
+# EOF
+
+
+# separate session to ignore foreign key errors
+# cat << EOF | sqlite3 -init "" "$SQLITEDB" 2> /dev/null || true
+# $SQLITE_INIT
+
+# SELECT "importing user_languages...";
+# CREATE TABLE user_languages(
+#   lang TEXT NOT NULL,
+#   skill_level INTEGER NOT NULL,
+#   username TEXT NOT NULL,
+#   details TEXT
+# );
+
+# .mode ascii
+# .separator "\t" "\n"
+# .import '$TRANS_OUT/user_languages.tsv' user_languages
+
+# -- CREATE INDEX user_languages_lang_idx ON user_languages (lang);
+# -- CREATE INDEX user_languages_skill_level_idx ON user_languages (skill_level);
+# -- CREATE INDEX user_languages_username_idx ON user_languages (username);
+# EOF
 
 
 cat << EOF | sqlite3 -init "" "$SQLITEDB"
@@ -191,14 +191,14 @@ SELECT "Deduplicating sentences...";
 SELECT "  links...";
 INSERT OR IGNORE INTO links (sentenceid, translationid) WITH duplicates as (select min(sentenceid) as minid, sentence from sentences group by sentence,lang having count(*) > 1) SELECT duplicates.minid as sentenceid, l.translationid FROM duplicates JOIN sentences s ON s.sentence = duplicates.sentence JOIN links l ON l.sentenceid = s.sentenceid WHERE s.sentenceid != duplicates.minid;
 
-SELECT "  tags...";
-INSERT OR IGNORE INTO tags (sentenceid, tag) WITH duplicates as (select min(sentenceid) as minid, sentence from sentences group by sentence,lang having count(*) > 1) SELECT duplicates.minid as sentenceid, t.tag FROM duplicates JOIN sentences s ON s.sentence = duplicates.sentence JOIN tags t ON t.sentenceid = s.sentenceid WHERE s.sentenceid != duplicates.minid;
+-- SELECT "  tags...";
+-- INSERT OR IGNORE INTO tags (sentenceid, tag) WITH duplicates as (select min(sentenceid) as minid, sentence from sentences group by sentence,lang having count(*) > 1) SELECT duplicates.minid as sentenceid, t.tag FROM duplicates JOIN sentences s ON s.sentence = duplicates.sentence JOIN tags t ON t.sentenceid = s.sentenceid WHERE s.sentenceid != duplicates.minid;
 
-SELECT "  sentences_with_audio...";
-INSERT OR IGNORE INTO sentences_with_audio (sentenceid, username, license, attribution_url) WITH duplicates as (select min(sentenceid) as minid, sentence from sentences group by sentence,lang having count(*) > 1) SELECT duplicates.minid as sentenceid, a.username, a.license, a.attribution_url FROM duplicates JOIN sentences s ON s.sentence = duplicates.sentence JOIN sentences_with_audio a ON a.sentenceid = s.sentenceid WHERE s.sentenceid != duplicates.minid;
+-- SELECT "  sentences_with_audio...";
+-- INSERT OR IGNORE INTO sentences_with_audio (sentenceid, username, license, attribution_url) WITH duplicates as (select min(sentenceid) as minid, sentence from sentences group by sentence,lang having count(*) > 1) SELECT duplicates.minid as sentenceid, a.username, a.license, a.attribution_url FROM duplicates JOIN sentences s ON s.sentence = duplicates.sentence JOIN sentences_with_audio a ON a.sentenceid = s.sentenceid WHERE s.sentenceid != duplicates.minid;
 
-SELECT "  deleting duplicate sentences...";
-DELETE FROM sentences WHERE sentenceid IN (WITH duplicates as (select min(sentenceid) as minid, sentence from sentences group by sentence,lang having count(*) > 1) SELECT sentenceid FROM duplicates JOIN sentences s ON s.sentence = duplicates.sentence WHERE sentenceid != duplicates.minid);
+-- SELECT "  deleting duplicate sentences...";
+-- DELETE FROM sentences WHERE sentenceid IN (WITH duplicates as (select min(sentenceid) as minid, sentence from sentences group by sentence,lang having count(*) > 1) SELECT sentenceid FROM duplicates JOIN sentences s ON s.sentence = duplicates.sentence WHERE sentenceid != duplicates.minid);
 
 
 
